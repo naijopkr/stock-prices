4fa671cc5035404c55d95d2a803842979d1a9935
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-05-20 14:17:52 -0500

    fetch data and create dataframe

--- __main__.py
+++ __main__.py
@@ -1,2 +1,36 @@
-if __name__ == '__main__':
-    print('Hello world')
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pandas_datareader import data, wb
+from datetime import datetime
+
+start = datetime(2006, 1, 1)
+end = datetime(2016, 1, 2)
+source = 'yahoo'
+tickers = [
+    'BAC',
+    'C',
+    'GS',
+    'JPM',
+    'MS',
+    'WFC'
+]
+
+def fetch_data(symbol):
+    return data.DataReader(symbol, source, start, end)
+
+def get_head(ds):
+    print(ds.head())
+    print()
+
+stock_data = dict()
+for ticker in tickers:
+    stock_data[ticker] = fetch_data(ticker)
+
+for stock_ds in stock_data:
+    get_head(stock_data[stock_ds])
+
+bank_stocks = pd.concat(stock_data, axis=1)
+bank_stocks.columns.names = ['Bank Ticker', 'Stock Info']
+get_head(bank_stocks)
